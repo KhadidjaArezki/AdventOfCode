9b8098db527739495f78ce608778ad7f58024ab7
Khadidja <khadidjaarezki999@gmail.com>
2024-12-09 18:04:51 +0100

    Add Day 7 part 2 solution

---
+++ AoC-2024/Day07/part2-solution.js
@@ -0,0 +1,36 @@
+fs = require("fs")
+
+function applySeq(opSeq, nums) {
+  let r = nums[0]
+  for (let k = 1; k < nums.length; k++) {
+    if (opSeq[k-1] == "+") r += nums[k]
+    else if (opSeq[k-1] == "*") r *= nums[k]
+    else r = parseInt(String(r) + String(nums[k]))
+  }
+  return r
+}
+
+const combinations = (arr, depth) => {
+  if (depth === 1) return arr
+  return combinations(arr, depth - 1).flatMap((val) =>
+    arr.map((char) => val + char)
+  )}
+
+fs.readFile("./input.txt", (e, data) => {
+  let ops = ["+", "*", "|"]
+  let sum = 0
+
+  data.toString().split(/\n/).forEach( e => {
+    let [r, nums] = e.split(": ")
+    r = parseInt(r)
+    nums = nums.split(/\s/).map(n => parseInt(n))
+    const opCombs = combinations(ops, nums.length - 1)
+    for (const opSeq of opCombs) {
+      if (applySeq(opSeq, nums) == r) {
+        sum += r
+        break
+      }
+    }
+  })
+  console.log(sum)
+})
