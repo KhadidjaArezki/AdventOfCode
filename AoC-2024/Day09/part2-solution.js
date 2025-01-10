2013e374834dbcb9e10dabdb19d0bfaee34e897b
Khadidja <khadidjaarezki999@gmail.com>
2025-01-10 19:29:55 +0100

    Add Day 9 part 2 solution

--- AoC-2024/Day09/part2-solution.js
+++ AoC-2024/Day09/part2-solution.js
@@ -1,20 +1,8 @@
 fs = require("fs")
 
 const sum = (arr) => arr.reduce((acc, n) => acc+n, 0)
-const get1stIdxOfAtLeast = (map , num, pos) => {
-  for (const k in map) {
-    if (k >= num) {
-      let idx = Math.min(...map[k])
-      if (idx > pos) break
-      map[k].splice(map[k].indexOf(idx), 1)
-      if (map[k].length == 0) delete map[k]
-      return [k, idx]
-    }
-  }
-  return false
-}
 
-fs.readFile("./input.txt", (e, data) => {
+fs.readFile("./sinput.txt", (e, data) => {
   string = data.toString()
   
   let id_counts = []
@@ -23,85 +11,48 @@ fs.readFile("./input.txt", (e, data) => {
     if (i % 2 == 0) id_counts.push(parseInt(string[i]))
     else freeSpaceCounts.push(parseInt(string[i]))
   }
-
-  let freeSpaceMap = {}
-  let freeSpacePos = id_counts[0]
-  for (let i = 0; i < freeSpaceCounts.length; i++) {
-    if (freeSpaceCounts[i] > 0) {
-      if (freeSpaceCounts[i] in freeSpaceMap) {
-       freeSpaceMap[freeSpaceCounts[i]].push(freeSpacePos)
-      } else {
-        freeSpaceMap[freeSpaceCounts[i]] = [freeSpacePos]
-      }
-      freeSpacePos += freeSpaceCounts[i] + id_counts[i+1]
-    }
-  }
   
   let numBlocks = sum(id_counts) + sum(freeSpaceCounts)
   let rear_pos = numBlocks-1
   let checkSum = 0
   let k = id_counts.length-1
+  let m = k-1
+  let freeSpaceCountsCopy = freeSpaceCounts.slice(0, freeSpaceCounts.length)
+  
   while (k >= 0) {
-    let result = get1stIdxOfAtLeast(freeSpaceMap, id_counts[k], rear_pos)
-    
-    if (!result) {
-      for (let l = 0; l < id_counts[k]; l++) {
-        checkSum += (k * rear_pos)
-        rear_pos--
+    let foundIdx = false
+    let idx
+    let freeSpaceCountIdx
+    for (let i = 0; i < k; i++) {
+      if (freeSpaceCountsCopy[i] >= id_counts[k]) {
+        idx = sum(id_counts.slice(0, i+1).concat(freeSpaceCounts.slice(0, i))) + 
+              (freeSpaceCounts[i] - freeSpaceCountsCopy[i])
+        
+        freeSpaceCountIdx = i
+        foundIdx = true
+        break
       }
-    } else {
-      let [freeSpaceCount, idx] = result
-      let newfreeSpaceCount = freeSpaceCount
+    }
+    
+    if (foundIdx) {
+      let newfreeSpaceCount = freeSpaceCountsCopy[freeSpaceCountIdx]
+      
       for (let l = 0; l < id_counts[k]; l++) {
         checkSum += (k * idx)
         newfreeSpaceCount--
         idx++ 
         rear_pos--
       }
-      if (newfreeSpaceCount > 0) {
-        if (newfreeSpaceCount in freeSpaceMap) freeSpaceMap[newfreeSpaceCount].push(idx)
-        else freeSpaceMap[newfreeSpaceCount] = [idx]
-      }
-    }
-    if (k >= 0) rear_pos -= freeSpaceCounts[k-1]
-    k--
-    // console.log(checkSum)
-  }
-  console.log(checkSum)
-  
-  /*
-  while (pos < numBlocks) {
-    let done = false
-    for (let p = 0; p < id_counts[m]; p++) {
-      if (pos >= numBlocks) {
-        done = true
-        break
-      }
-      checkSum += (m * pos)
-      pos++
-    }
-    let newfreeSpaceCount = freeSpaceCounts[m]
-    if (id_counts[k] <= freeSpaceCounts[m]) {
-      for (let l = 0; l < freeSpaceCounts[m]; l++) {
-        if (pos >= numBlocks) {
-          done = true
-          break
-        }
-        checkSum += (k * pos)
-        newfreeSpaceCount--
-        id_counts[k]--
-        pos++ 
-      }
-      k--
+      freeSpaceCountsCopy[freeSpaceCountIdx] = newfreeSpaceCount
     } else {
-
-    }
-    if (done) break
-    freeSpaceCounts[m] = newfreeSpaceCount
-    if (freeSpaceCounts[m] == 0) {
-      m++
-    }
+      for (let l = 0; l < id_counts[k]; l++) {
+        checkSum += (k * rear_pos)
+        rear_pos--
+      }
+    } 
+    rear_pos -= freeSpaceCounts[m]
+    k--
+    m--
   }
   console.log(checkSum)
-  */
 })
