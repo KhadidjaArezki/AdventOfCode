f53ae58dbbb74230c552f2cab607e406cfd7c283
Khadidja <khadidjaarezki999@gmail.com>
2025-01-25 17:28:07 +0100

    Add Day 10 part 2 solution

---
+++ AoC-2024/Day10/part2-solution.js
@@ -0,0 +1,39 @@
+fs = require("fs")
+
+fs.readFile("./input.txt", (e, data) => {
+  let scoresSum = 0
+  let lines = data.toString().split(/\n/)
+  lines.forEach((line, i) => {
+    let trailHeads = []
+    for (let j = 0; j < line.length; j++) {
+      if (line[j] === "0") trailHeads.push(j)
+    }
+
+    for (let t of trailHeads) {
+      let trailPositions = [[i, t]]
+      currentTrailHeadVal = 0
+      while (currentTrailHeadVal < 9) {
+        currentTrailHeadVal += 1
+        let newTrailPositions = []
+        for (const pos of trailPositions) {
+          const [y, x] = pos
+          if (x > 0 && lines[y][x-1] == currentTrailHeadVal) {
+            newTrailPositions.push([y, x-1])
+          }
+          if (x < lines[y].length-1 && lines[y][x+1] == currentTrailHeadVal) {
+            newTrailPositions.push([y, x+1])
+          }
+          if (y > 0 && lines[y-1][x] == currentTrailHeadVal) {
+            newTrailPositions.push([y-1, x])
+          }
+          if (y < lines.length-1 && lines[y+1][x] == currentTrailHeadVal) {
+            newTrailPositions.push([y+1, x])
+          }
+        }
+        trailPositions = newTrailPositions
+      }
+      scoresSum += trailPositions.length
+    }
+  })
+  console.log(scoresSum)
+})
